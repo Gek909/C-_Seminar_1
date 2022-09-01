4a17c7dd9584c0b905073b2a27b049411b665e25
Evgeniy <gek909@mail.ru>
2022-09-01 19:17:09 +0300

    1st program x^2 done

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая выдает квадрат числа
+
+// Определение входного числа
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+// Получение квадрата входного числа
+int result = number * number;
+Console.WriteLine($"Квадрат числа {number} = {result}");
+
+// Получение квадрата числа математический функции
+int result_2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} = {result_2}");
