c1a2d7fe71a25825b15472269bc0cc508ed0a3a0
Evgeniy <gek909@mail.ru>
2022-09-02 19:13:34 +0300

    8th exercise 1 to N even number sequence done

---
+++ Homework/Exercise8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int startNumber = 2;
+
+while (startNumber <= number)
+{
+        Console.WriteLine(startNumber);
+        startNumber = startNumber + 2;
+}
