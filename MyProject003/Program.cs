04b77d4d099047358df341e6c62f26857cb901af
Evgeniy <gek909@mail.ru>
2022-09-02 13:34:29 +0300

    4th program number +- range added

---
+++ MyProject003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определение входного числа
+
+Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+
+int startNumber = Math.Abs(number) * -1;
+
+while (startNumber <= number) 
+{
+    Console.WriteLine(startNumber);
+    startNumber++;
+}
+
+    
