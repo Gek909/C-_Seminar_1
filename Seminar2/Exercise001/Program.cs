87cd79b9de54a4ba33a328b3512a168be42235d6
Evgeniy <gek909@mail.ru>
2022-09-06 13:50:58 +0300

    1st exercise added

---
+++ Seminar2/Exercise001/Program.cs
@@ -0,0 +1,13 @@
+﻿void CompareDigits ()
+    {
+    int number = new Random().Next(10, 100);
+    int number1 = number / 10;
+    int number2 = number % 10;
+    Console.WriteLine($"Сгенерированно случайное число {number}");
+    if (number1 > number2) Console.WriteLine("Первая цифра больше");
+    else if (number2 > number1) Console.WriteLine("Вторая цифра больше");
+    else Console.WriteLine("Это паллиндром");
+    }
+
+CompareDigits ();
+CompareDigits ();
