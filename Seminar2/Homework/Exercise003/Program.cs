52959adb84fc8f0ac4af4c5fcb3b8b5c992bd70c
Evgeniy <gek909@mail.ru>
2022-09-07 16:27:18 +0300

    2nd seminar homework added full

--- Seminar2/Homework/Exercise003/Program.cs
+++ Seminar2/Homework/Exercise003/Program.cs
@@ -4,7 +4,8 @@
 void WeekendCheck(int num)
 {
     if ((num >= 1) & (num < 6)) Console.WriteLine("Не выходной");
-    if ((num == 6) | (num == 7)) Console.WriteLine("Выходной");  
+    else if ((num == 6) | (num == 7)) Console.WriteLine("Выходной");  
+    else Console.WriteLine("Введите число от 1 до 7");
 }
 
 
