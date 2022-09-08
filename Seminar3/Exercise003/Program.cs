d66fc32899c8c5707a7223ed509a2d918981d5aa
Evgeniy <gek909@mail.ru>
2022-09-08 12:48:13 +0300

    3rd seminar 3 exercises done (full)

---
+++ Seminar3/Exercise003/Program.cs
@@ -0,0 +1,30 @@
+﻿// Версия с массивом и методом:
+
+int[] SquaresTable(int num)
+{
+    int[] squareNumbers = new int[num];
+
+    // Заполнение массива результата
+    for (int i = 1; i <= num; i++)
+    {
+        squareNumbers[i - 1] = i * i;
+    }
+    return squareNumbers;
+}
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] result = SquaresTable(num);
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
+
+// Рабочая версия без метода (самая простая):
+//for (int i = 1; i <= num; i++)
+//    {
+//        int result = i*i;
+//        Console.Write($"{result},");
+//    }
